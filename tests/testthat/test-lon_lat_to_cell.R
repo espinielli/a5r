ab25e4de8f0b48c1b8bf959c8c37044428291208
Enrico Spinielli <enrico.spinielli@eurocontrol.int>
2026-01-08 16:51:08 +0100

    remove comments

--- tests/testthat/test-lon_lat_to_cell.R
+++ tests/testthat/test-lon_lat_to_cell.R
@@ -7,7 +7,4 @@ test_that("lon_lat_to_cell works correctly", {
 
   ll <- lon_lat_to_cell(lon = -0.1597184, lat = 51.5118429, 10)
   expect_equal(cell, 7161034019553935360)
-
-  # cell <- lon_lat_to_cell(4.4260, 50.8433, 14) # 0x63c7b85580000000
-  # expect_equal(cell, 7189918007479500800)
 })
